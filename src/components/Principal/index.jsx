e6a6bef13e8eb95736ebbec84de7f50c71bf8c28
Denis Jeronimo Viana Filho <41395264+devdenisviana@users.noreply.github.com>
2024-12-07 18:04:18 -0300

    feat: criacao do principal

--- src/components/Principal/index.jsx
+++ src/components/Principal/index.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import styles from './styles.module.css'
-// import Airplane from '../../assets/imagens/airplane.svg';
+import React from "react";
+import styles from "./styles.module.css";
+
 
 export default function Principal() {
-    return(
-        <>
-        <main>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-6'> 
-                        <p>Perca | Se</p>
-                        <p>Descubra | Se</p> 
+  return (
+    <>
+      <main>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className={styles.conteudoPrincipal}>
+                          <p className={styles.planos}><a href="#">Planos terrestres</a></p>
+                          <p className={styles.planos}><a href="#">Planos maritimos</a></p>
+                          <p className={styles.planos}><a href="#">Planos áreos</a></p>
                     </div>
                 </div>
             </div>
-        </main>
-        </>
-    )
+        </div>
+      </main>
+    </>
+  );
 }
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/index";
 import Principal from "../../components/Principal/index";
+import Footer from "../../components/Footer/index";
 
 export default function Home() {
     return(
         <>
         <Header />
         <Principal />
+        <Footer />
         </>
     )
 }
